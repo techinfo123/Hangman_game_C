b313bf2973d2e97ec2db96e398c6123b38eb423c
Atishay_Jain_2005 <128222556+techinfo123@users.noreply.github.com>
2024-02-18 17:57:54 +0530

    Add files via upload

---
+++ helloworld2.c
@@ -0,0 +1,443 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include<ctype.h>
+
+void gameFunction(char *wordinFunction, char *userWordinFunction, int letterNumber);   //Function for checking the letter, incrementing counters etc.
+
+void printUserWord(char * userWordinPrint,int letterNumber);			       //Function for printing the hanmgan figure 
+
+void printFigure(int failinFigure);						       //Function for printing the array of user
+
+int main()
+{
+	
+
+	char readFromFile[]={'B','A','N','A','N','A',
+
+	'0','C','A','R','R','O','T','0','G','R','A',
+
+	'P','E','S','0','O','R','A'
+
+	,'N','G','E','0','T','O','M','A','T','O','0'
+
+	,'A','P','R','I','C','O','T','L','E','T','T',
+
+	'U','C','E','P','U','M','P','K','I','N','A',
+
+	'V','O','C','A','D','O','C','A','B','B','A',
+
+	'G','E','S','O','C','C','E','R','0','T','E',
+
+	'N','N','I','S','0','K','A','R','A','T','E',
+
+	'0','S','Q','U','A','S','H','0','B','O','X',
+
+	'I','N','G','0','C','R','I','C','K','E','T',
+
+	'F','E','N','C','I','N','G','R','U','N','N',
+
+	'I','N','G','S','U','R','F','I','N','G','R',
+
+	'A','F','T','I','N','G','C','A','M','E','R',
+
+	'A','0','L','A','P','T','O','P','0','F','R',
+
+	'I','D','G','E','0','T','A','B','L','E','T',
+
+	'0','W','A','S','H','E','R','0','M','O','N',
+
+	'I','T','O','R','P','R','I','N','T','E','R',
+
+	'B','A','T','T','E','R','Y','S','P','E','A',
+
+	'K','E','R','S','C','A','N','N','R','T','U',
+
+	'R','K','E','Y','0','F','R','A','N','C','E',
+
+	'0','R','U','S','S','I','A','0','B','R','A',
+
+	'Z','I','L','0','C','A','A','D','A','0','G',
+
+	'E','R','M','A','N','Y','E','N','G','L','A',
+
+	'N','D','D','E','N','M','A','R','K','U','K',
+
+	'R','A','I','N','E','H','U','N','G','A','R','Y'};
+	
+	 //an array is created that conatins all the name being used in the game.
+	
+	int restartGame= 1; //At the end, that variable's value can change so the game terminates
+
+	while(restartGame==1)
+	{
+		int gameType;   //Program asks user to letter size
+
+		printf(" \t\t WELCOME TO HANGMAN!\n\n \t\tChoose a game\n \t\t1. Six Letters Game\n \t\t2. Seven Letters Game\n \t\t3. choose for me\n \t\tEnter your choice: ");
+
+		scanf("%d", &gameType);
+	
+		while(gameType != 1 && gameType != 2 && gameType !=3)
+		{
+			system("cls");
+
+			printf(" \t\t WELCOME TO HANGMAN!\n\n \t\tChoose a game\n \t\t1. Six Letters Game\n \t\t2. Seven Letters Game\n \t\t3. choose for me\n \t\tEnter your choice: ");
+
+			scanf("%d", &gameType);
+		}
+	
+		system("cls");
+	
+		if(gameType == 3)
+		{
+			gameType = rand() % 2 + 1;
+		}
+	
+		int categoryType; //Program asks user to category type
+
+		printf("\n\nChoose a category\n1. Fruits and Vegetables\n2. Sports\n3. Technological Devices\n4. Countries\n5. choose for me\nEnter your choice:  ");
+	
+		scanf("%d", &categoryType);
+	
+		while(categoryType != 1 && categoryType != 2 && categoryType != 3 && categoryType != 4 && categoryType != 5)
+		{
+			system("cls");
+
+			printf("\n\nChoose a category\n1. Fruits and Vegetables\n2. Sports\n3. Technological Devices\n4. Countries\n5. choose for me\nEnter your choice:  ");	
+
+			scanf("%d", &categoryType);
+		}
+	
+		if(categoryType== 5)
+		{
+			categoryType= rand() % 4 + 1;
+		}
+	
+		char askedWord[8];									//Array for holding the word
+
+		char userWord[8]= {'_','_','_','_','_','_','_'};    //Array for user's true letters
+		
+		int randomer = rand()% 5;                           //Program writes appropriate random word to askedWord array from readed file
+		
+		if(categoryType == 1)      //Category for Fruits and Vegetables
+
+		{	
+
+			if(gameType== 1)
+
+			{
+				for(int i= 0; i< 7; i++)
+				{
+
+					askedWord[i]= readFromFile[(35* 0)+ (randomer * 7)+ i];
+
+				}
+				
+			}
+			
+			else
+			{
+				for(int i= 0; i< 7; i++)
+				{
+
+					askedWord[i]= readFromFile[(35* 1)+ (randomer * 7)+ i];
+
+				}
+				
+                
+			}
+		}
+	
+
+
+		else if(categoryType == 2) //Category for Sports
+		{		
+			if(gameType==1)
+			{
+				for(int i= 0; i< 7; i++)
+				{
+
+
+					askedWord[i]= readFromFile[(35* 2)+ (randomer * 7)+ i];
+				}
+				
+			}
+			
+			else
+			{
+				for(int i= 0; i< 7; i++)
+				{
+					askedWord[i]= readFromFile[(35* 3)+ (randomer * 7)+ i];
+				}
+				
+			}
+		}
+	
+		else if(categoryType == 3) //Category for Technological Devices
+		{
+			if(gameType==1)
+			{
+				for(int i= 0; i< 7; i++)
+
+				{
+					askedWord[i]= readFromFile[(35* 4)+ (randomer * 7)+ i];
+				}
+				
+			}
+			
+			else
+			{
+				for(int i= 0; i< 7; i++)
+				{
+					askedWord[i]= readFromFile[(35* 5)+ (randomer * 7)+ i];
+				}
+				
+			}
+		}
+
+
+		else			        //Category for Countries
+		{
+			if(gameType==1)
+			{
+				for(int i= 0; i< 7; i++)
+				{
+					askedWord[i]= readFromFile[(35* 6)+ (randomer * 7)+ i];
+				}
+				
+			}
+			
+			else
+			{
+				for(int i= 0; i< 7; i++)
+				{
+					askedWord[i]= readFromFile[(35* 7)+ (randomer * 7)+ i];
+				}
+				
+			}
+		}
+		
+		gameFunction(askedWord, userWord, (gameType+ 5));    //Program goes to gameFunction with neccessary arrays and lettter size
+		
+		printf("\n\nEnter 1 to restart game\nEnter any other number to exit\n");    //At the end of the game program asks user to replay or not
+
+
+		scanf(" %d", &restartGame);
+
+		system("cls");
+	}
+}
+
+
+
+
+
+void gameFunction(char *wordinFunction, char *userWordinFunction, int letterNumber)      //Function for checking the letter, incrementing counters etc.
+{
+	int totalTrue= 0;			//Total number of true entered letters -if equals to letterNumber game is over-
+
+	int totalFail= 0;			//Total number of fails -must be less than 9 or game is over-
+
+	char scannedChar;
+	
+	while(totalFail< 9)
+	{
+		printFigure(totalFail);		//Program goes function to print the corresponding figure to fail
+
+		printf("\n\n\n");
+
+		printUserWord(userWordinFunction, letterNumber);     //Program goes function to print the user's array
+
+		printf("\nEnter a character: ");
+
+		scanf(" %c", &scannedChar);
+
+		scannedChar= toupper(scannedChar);
+	
+		int letterAlreadyUsed= 1;	
+				//Program checks whether the inputted letter is already found or not
+		while(letterAlreadyUsed== 1)
+
+		{
+			
+			for(int i=  0; i< letterNumber; i++)
+
+			{
+		
+				if(userWordinFunction[i]!= scannedChar)
+
+				{
+					letterAlreadyUsed= 0;	
+				}
+				
+				else
+				{
+					letterAlreadyUsed= 1;
+
+					printFigure(totalFail);
+
+					printf("\n\n\n");
+
+					printUserWord(userWordinFunction, letterNumber);
+
+					printf("\nLetter already found!\nEnter a new character: ");
+
+					scanf(" %c", &scannedChar);
+				
+				
+					scannedChar= toupper(scannedChar);
+
+					break;
+				}
+				
+			
+
+			}
+		
+		}
+	
+		int letterTrueCheck= 0;       //This part checks every valid letter in order to understand...
+
+		for(int i= 0; i< letterNumber; i++)			   //... it is a part of askedWord or not with counters
+
+		{
+			if(scannedChar== wordinFunction[i])		   //This part means that user entered a correct character
+
+			{
+				userWordinFunction[i]= wordinFunction[i];
+
+				letterTrueCheck++;
+
+				totalTrue++;				
+			}
+		}
+		
+		if(letterTrueCheck== 0)						//This part means that user entered a wrong character
+
+		{
+			totalFail++;
+		}
+		
+		if(totalTrue==letterNumber)                //This if statement works if you win the game and returns to main
+
+		{
+			printFigure(totalFail);
+
+			printf("\n\n\n");
+
+			printUserWord(userWordinFunction, letterNumber);
+
+			printf("\n\nYOU WON!");
+
+			return;
+		}
+
+	}
+	
+	printFigure(totalFail);	                         //This part works if you loose the game and returns to main
+
+	printf("\n\n\n");
+
+	for(int i= 0; i< letterNumber; i++)
+
+	{
+		printf("%c ", wordinFunction[i]);
+
+	}
+
+	printf("\n\nYOU LOST!");	
+
+	return;
+	
+}
+
+void printUserWord(char *userWordinPrint,int letterNumber)    //This function basicly prints the array of user
+
+{
+	for(int i= 0; i< letterNumber; i++)
+
+	{
+
+		printf("%c ", userWordinPrint[i]);
+
+	}
+}
+
+void printFigure(int failinFigure)    //This function clears the screen and prints the corresponding fail figure
+
+{
+	system("cls");
+
+	
+	switch(failinFigure)
+
+	{
+
+		case 0:
+
+			printf("\n\n\n\n\n        \n        \n        \n     __|__");
+
+			break;
+
+		case 1:
+
+			printf("\n\n\n\n\n       |\n       |\n       |\n     __|__");
+
+			break;
+
+		case 2:
+
+			printf("  _ _ _\n       |\n       |\n       |\n       |\n       |\n       |\n       |\n     __|__ ");
+
+			break;
+
+		case 3:
+
+			printf("  _ _ _  \n |     | \n |     | \n       |\n       |\n       |\n       |\n       |\n     __|__ ");
+
+			break;
+
+		case 4:
+
+			printf("  _ _ _  \n |     | \n |     | \n( )    |\n       |\n       |\n       |\n       |\n     __|__ ");
+
+			break;
+
+		case 5:
+
+			printf("  _ _ _  \n |     | \n |     | \n( )    |\n/      |\n       |\n       |\n       |\n     __|__ ");
+
+			break;
+
+		case 6:
+
+			printf("  _ _ _  \n |     | \n |     | \n( )    |\n/ \\    |\n       |\n       |\n       |\n     __|__ ");
+
+			break;
+
+		case 7:
+
+			printf("  _ _ _  \n |     | \n |     | \n( )    |\n/|\\    |\n |     |\n |     |\n       |\n     __|__ ");
+
+			break;
+
+		case 8:
+
+			printf("  _ _ _  \n |     | \n |     | \n( )    |\n/|\\    |\n |     |\n |     |\n/      |\n     __|__ ");
+
+			break;
+
+		case 9:
+
+			printf("  _ _ _  \n |     | \n |     | \n( )    |\n/|\\    |\n |     |\n |     |\n/ \\    |\n     __|__ ");
+
+			break;
+
+	}
+	
+	return ;
+
+}	
+
+    
+
